275700d27080286199c3ac87a99c90706266188e
Matt Colf <mattcolf@mattcolf.com>
2012-01-10 12:34:49 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+#Multi-Part PHP Download Script
+
+This script provides a download path for files that are not within the web root. For example, you could
+host files from /w/media when your web root is /w/web, therefor giving you more control over your media
+file downloads (security, statistics, etc.). It supports multi-part downloads.
+
+##FEATURES
+
+- Multipart download support (allows multi-threaded client downloads). 
+- Download resume after failure.
+- Very large file support. Files are split into chunks for download, limiting memory usage.
+- Key based download. Hides file path information from browser. 
+
+##DEPENDENCIES
+
+- Apache setup and configured with PHP and .htaccess support.
+- Apache must support URL rewrites.
+- $content_base and $default_content must be included in PHP open_base() path.
+- PHP must be able to run code via exec() [PHP filesize() int overflow workaround]
+- The 'file' command must installed (on most Linux systems by default) [mime type detection]
+
+##USAGE
+
+This script tricks the web browser/downloader into thinking that the file being downloaded
+is a simple directory based download. Here's how it works.
+
+1. Define your $content_base. If the file you want to download is in /w/media/file.mp4, then your 
+$content_base would be /w/media/.
+
+2. Setup your download directory. Drop the entire download directory into your web root. For example, if
+your web root is /w/web then your download directory could be /w/web/download. So, now you can call the 
+download script by pointing your browser at http://{hostname}/download.
+
+3. Create a download key. Within the page you are going to link from, take the full path of the file you
+want to download, remove the $content_base (i.e. /w/media/file.mp4 becomes file.mp4) and encode the string 
+in PHP with base64_encode($string). This is your download key. 
+
+4. Create a link to the download script. You can down combine everything into a working download link. 
+It should be in the following format. You can change this by modifying the rewrite rule in the .htaccess
+file.
+`````````````````````````````````````````````````````````````````````````````
+http://{hostname}/download/key/{download_key}/name/{file_name}.{file_extension}
+`````````````````````````````````````````````````````````````````````````````
+
+5. Display the link to a user, pass it to a media player, or do whatever else you need to do. 
---
+++ download/index.php
@@ -0,0 +1,97 @@
+<?php 
+
+// define content path including trailing slash
+// for example, if your files are in /w/media (/w/media/file.mp4), then your content base would be /w/media/
+$content_base;
+
+// full path of default content
+// if this script is called with no parameters, then this file is provided 
+$default_content;
+
+// disable caching
+header("Pragma: public");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Cache-Control: private");
+
+// disable output buffering
+@ob_end_clean();
+
+// disable execution time limit
+set_time_limit(0);
+
+// disable compression (for IE)
+if(ini_get('zlib.output_compression')) ini_set('zlib.output_compression', 'Off');
+
+// decode the requested file name
+if ( isset($_REQUEST["key"]) ) {
+	if ( $file = base64_decode($_REQUEST["key"],TRUE) ) {
+		$file = $content_base.$file;
+	}
+	else die("Invalid file key!");
+} else $file = $default_content;
+
+// check for valid file
+if ( !file_exists($file) ) die("File does not exist!");
+
+// detect mime type
+$mime = exec("/opt/bin/file -i -b '".$file."'");
+if ( $clean = strstr($mime,";",TRUE) ) {
+	$mime = $clean;
+}
+if ( strlen($mime) < 1 ) die("Invalid MIME type.");
+
+// file specific headers
+header('Accept-Ranges: bytes');
+header("Content-Description: File Transfer");
+header("Content-Type: $mime");
+header('Content-Disposition: attachment; filename="'.basename($file).'"');
+header("Content-Transfer-Encoding: binary");
+
+// determine file size
+$size = filesize($file);
+// workaround for int overflow
+if ( $size < 0 ) {
+	$size = exec('ls -al "'.$file.'" | awk \'BEGIN {FS=" "}{print $5}\'');
+}
+			
+// add multipart download and resume support			
+if ( isset($_SERVER["HTTP_RANGE"]) ) {
+	list($a,$range) = explode("=",$_SERVER["HTTP_RANGE"],2);
+	list($range) = explode(",",$range,2);
+	list($range,$range_end) = explode("=",$range);
+	$range = round(floatval($range),0);
+	if ( !$range_end ) $range_end = $size-1;
+	else $range_end = round(floatval($range_end),0);
+
+	$partial_length = $range_end-$range+1;
+	header("HTTP/1.1 206 Partial Content");
+    header("Content-Length: $partial_length");
+    header("Content-Range: bytes ".($range-$range_end/$size));
+}
+else {
+	$partial_length = $size;
+	header("Content-Length: $partial_length");
+}
+
+// output file
+$chunksize = 1*(1024*1024);
+$bytes_send = 0;
+if ( $fp = fopen($file,"r") ) {
+	// fast forward within file, if requested
+	if (isset($_SERVER['HTTP_RANGE'])) fseek($fp,$range);
+	// read and output the file in chunks
+	while( !feof($fp) AND (!connection_aborted()) AND ($bytes_send < $partial_length) ) {
+		$buffer = fread($fp,$chunksize);
+		print($buffer);
+		flush();
+		$bytes_send += strlen($buffer);
+	}
+	fclose($fp);
+}
+else die("Unable to open file.");
+
+// end the script
+exit();
+
+?>
